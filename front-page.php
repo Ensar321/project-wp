ce4d22178e7e04914621e44f5f79593a303ce30f
Ensar321 <ensarpushkolii12@gmail.com>
2026-03-14 14:38:52 +0100

    ,N2HAEWFJ

---
+++ front-page.php
@@ -0,0 +1,68 @@
+<?php get_header();?>
+<main>
+    <section class='hero'>
+        <img class='hero-banner'
+        src='<?php echo esc_url(get_theme_file_url('beveragebanner.png'));?>'
+        alt='site banner'>
+</section>
+
+<section class ='features'>
+    <hr>
+    <h2 id ='heading'>Why choosing Us?</h2>
+    <br>
+
+    <ul>
+        <li>
+            <img src="<?php echo esc_url_(get_theme_file_url('Coffe.png'));?>"
+            alt="Foto1">
+            <span>Best Coffe in the world</span>
+        </li>
+    </ul>
+</section>
+<br>
+
+<section class='section-pad'>
+    <h3 id='curricula-heading'> What We Serve. </h3>
+    <br>
+
+    <p class='curricula-grid'>
+    
+        <ul class='card-points'>
+            <li>We serve coffe</li>
+            <li>Drinks of all sorts are a must!So you will find yourself a wide range of options.</li>
+            <li>There's a lot of </li>
+            <li>Group:8-16 students</li>      
+</p>
+</li>
+
+<li class='card'>
+            <h3 class ='card-title'>
+                <span class='label'>Curriculas for the group age</span>
+                <span class='age'>12-15 years</span>
+        </h3>
+        <ul class='card-points'>
+            <li>CONTENT:Learning the logivc amd basics of programing</li>
+            <li> Period:September-July(10 month academic year)</li>
+            <li>Intensity:3 academic Years</li>
+            <li>Group:8-16 students</li>
+            <li>Method:Online or physical</li>           
+</ul>
+</li>
+
+<li class='card'>
+            <h3 class ='card-title'>
+                <span class='label'>Curriculas for the group age</span>
+                <span class='age'>16-19 years</span>
+        </h3>
+        <ul class='card-points'>
+            <li>CONTENT:Learning the logivc amd basics of programing</li>
+            <li> Period:September-July(10 month academic year)</li>
+            <li>Intensity:3 academic Years</li>
+            <li>Group:8-16 students</li>
+            <li>Method:Online or physical</li>           
+</ul>
+</li>
+</section>
+</main>
+<?php get_footer();?>
+
